54835267ead2261cc0b76d59c4aacf368edbeddf
Mikhail Belianchikov <experimenter021@gmail.com>
2025-08-14 18:48:34 +0900

    Update setup.py
    Fix non-editable install and version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zeroheliumkit',
-    version='0.4.1',
-    packages=find_packages(include=['src', 'helpers', 'fem']), 
+    version='0.4.2',
+    packages=find_packages(include=['zeroheliumkit', 'src', 'helpers', 'fem']), 
     install_requires=[]
 )
